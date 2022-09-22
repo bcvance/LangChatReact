6de91455643f40620dd2db80df3ae47ac30bc93a
bcvance <81263222+bcvance@users.noreply.github.com>
2022-09-21 20:49:16 -0500

    can make api call to django backend

--- src/components/ConvosModal.js
+++ src/components/ConvosModal.js
@@ -1,12 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Form, ModalBody, Button, Nav, Tab } from 'react-bootstrap'
 import Modal from 'react-bootstrap/Modal'
+import axios from '../axiosConfig.js'
 
 function ConvosModal({show, setShow}) {
     const options = ['user1','user2','user3']
     const handleClose = () => setShow(false)
     const [activeTab, setActiveTab] = useState('findPartner')
     const languages = ['English', 'French', 'German', 'Russian', 'Spanish']
+    const [myLanguage, setMyLanguage] = useState('English')
+    const [learningLanguage, setLearningLanguage] = useState('English')
+
+    
+    // update state on form change
+    const updateLanguages = (event) => {
+        event.target.name === 'myLanguage' ? setMyLanguage(event.target.value) : setLearningLanguage(event.target.value)
+    }
+
+    
+    // make call to backend to match user with language partner and generate new chat room
+    const handleSubmit = (myLanguage, learningLanguage, event) => {
+        event.preventDefault()
+        let data = new FormData()
+        data.append('know-languages', myLanguage)
+        data.append('learning-languages', learningLanguage)
+        data.append('username', 'user1')
+        axios.post('http://127.0.0.1:8000/chat/', data)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
 
   return (
     <Modal show={show} onHide={handleClose}>
@@ -24,10 +50,10 @@ function ConvosModal({show, setShow}) {
             <ModalBody>
                 <Tab.Content>
                     <Tab.Pane eventKey='findPartner'>
-                        <Form>
+                        <Form onSubmit={(event) => handleSubmit(myLanguage, learningLanguage, event)}>
                             <Form.Group className='mb-3' controlId='myLanguage'>
                                 <Form.Label>I know:</Form.Label>
-                                <Form.Select>
+                                <Form.Select name='myLanguage' onChange={updateLanguages}>
                                     {languages.map((language, index) => (
                                         <option key={index} label={language}>{language}</option>
                                     ))}
@@ -35,7 +61,7 @@ function ConvosModal({show, setShow}) {
                             </Form.Group>
                             <Form.Group className='mb-3' controlId='learningLanguage'>
                                 <Form.Label>I'm learning:</Form.Label>
-                                <Form.Select>
+                                <Form.Select name='learningLanguage' onChange={updateLanguages}>
                                     {languages.map((language, index) => (
                                         <option key={index} label={language}>{language}</option>
                                     ))}
